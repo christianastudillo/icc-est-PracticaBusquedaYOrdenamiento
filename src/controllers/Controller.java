e0fd1094004726ad91e1820f99b7ffdfca7c3204
Christian Astudillo <chris.astu134@gmail.com>
2024-10-21 11:34:09 -0500

    first commit

---
+++ src/controllers/Controller.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import models.Person;
+import views.View;
+public class Controller {
+    private View view;
+    private SortingMethods sortingMethods;
+    private SearchMethods searchMethods;
+    private Person[] persons;
+    public Controller(View view, SortingMethods sortingMethods, SearchMethods searchMethods){
+
+    }
+}
